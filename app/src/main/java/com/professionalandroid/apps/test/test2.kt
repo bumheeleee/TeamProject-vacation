947718babe3248fe03ed458ca03428d470accb08
bumhee lee <hufs.dlqjagml6447@naver.com>
2020-07-06 20:05:31 +0900

    setOnClickListener를 이용한 버튼 동작

--- app/src/main/java/com/professionalandroid/apps/test/test2.kt
+++ app/src/main/java/com/professionalandroid/apps/test/test2.kt
@@ -2,10 +2,18 @@ package com.professionalandroid.apps.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_test2.*
+import kotlinx.android.synthetic.main.activity_test2.view.*
+import kotlinx.android.synthetic.main.activity_test2.view.btn_Login
 
 class test2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test2)
+        //val btnlogin:Button = findViewById(R.id.btn_Login)
+        btn_Login.setOnClickListener {
+            btn_Login.setText("범희")
+        }
     }
 }
