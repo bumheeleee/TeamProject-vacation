d7c69bb60eac93306e2780c44e4a45ac5f3b7b80
bumhee lee <hufs.dlqjagml6447@naver.com>
2020-07-09 13:41:13 +0900

    Fragment하는법

---
+++ app/src/main/java/com/professionalandroid/apps/test/test4.kt
@@ -0,0 +1,20 @@
+package com.professionalandroid.apps.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+
+class test4 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_test3,container,false)
+    }
+
+}
